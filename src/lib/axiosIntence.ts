8c23d82ad03a33b37844a2914d6fe77c4e65a77a
Hussein mursal <husseinmursal@Husseins-MacBook-Pro.local>
2025-09-26 21:01:40 +0300

    secret

--- src/lib/axiosIntence.ts
+++ src/lib/axiosIntence.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosIntence = axios.create({
-  baseURL: `http://localhost:6001/`,
+  baseURL: `https://posbacknedtest-git-main-somaliatechs-projects.vercel.app/`,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
